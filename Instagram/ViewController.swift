dc740cca9b201fdd5edb3f019f57d64ad9ea35c5
Furkan Kaan ugsuz <ugsuzf@gmail.com>
2020-08-19 15:21:49 +0200

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Furkan Kaan Ugsuz on 19/08/2020.
+//  Copyright © 2020 Furkan Kaan Ugsuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
